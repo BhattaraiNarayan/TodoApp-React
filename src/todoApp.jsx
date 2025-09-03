f57c81bb4ab566e20fe66479256a3c44adbe212b
Bhattarai Narayan <bhattarai.narayan@spiral-platform.co.jp>
2025-09-03 18:33:16 +0900

    state updated

--- src/todoApp.jsx
+++ src/todoApp.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function todoApp(){
-    let [todo, setTodo] =useState(["sample task"]);
+    let [todo, setTodo] = useState([{task:"starting list", id: uuidv4() }]);
+    let [newTodo, setNewTodo]= useState("");
+
+    let addNewTask = () =>{
+        setTodo([...todo, { task:newTodo, id: uuidv4()}]);
+        setNewTodo("");
+    }
     
+    let updateTodoValue = (event)=>{
+        setNewTodo(event.target.value);
+    }
+
     return(
-        <>
-     <input placeholder='Add a task list'></input>
-      <button>Create Task</button>
+      <>
+     <input placeholder='Add a task list' 
+     value={newTodo}
+     onChange={updateTodoValue}></input>
+      <button onClick={addNewTask}>Create Task</button>
       <br /><br />
       <br /><br />
       <br /><br />
@@ -14,7 +27,7 @@ export default function todoApp(){
       <ul>
         {
             todo.map((todoList)=>(
-                   <li>{todoList}</li> 
+                   <li key={todoList.id}>{todoList.task}</li> 
             ))
         }
       </ul>
