3a62856f02fe384bbc31794030c6e8200452511d
Bhattarai Narayan <bhattarai.narayan@spiral-platform.co.jp>
2025-09-03 16:04:48 +0900

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <input placeholder='Add a task list'></input>
-      <button>Create Task</button>
-      <br /><br />
-      <br /><br />
-      <br /><br />
-      <h4>List Item</h4>
-      <ul></ul>
+    <TodoApp />
     </>
   );
 }
--- src/todoApp.jsx
+++ src/todoApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function todoApp(){
+    let [todo, setTodo] =useState(["sample task"]);
+    
     return(
-        <div>
-            Todo React Application
-        </div>
-    )
+        <>
+     <input placeholder='Add a task list'></input>
+      <button>Create Task</button>
+      <br /><br />
+      <br /><br />
+      <br /><br />
+      <h4>List Item</h4>
+      <ul>
+        {
+            todo.map((todoList)=>(
+                   <li>{todoList}</li> 
+            ))
+        }
+      </ul>
+      </>
+    );
 }
